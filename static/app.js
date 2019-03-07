40477ff66ef8aeaf3269af5492d3ff8a9f23e07e
Yorrmungand <edgarmarques@hotmail.com.br>
2019-03-07 18:51:14 -0300

    Made Candle clickable

--- static/app.js
+++ static/app.js
@@ -1,30 +1,39 @@
 const createChart = () => {
-  
-  let guide = 0;
+
+  let guide = 400;
   let left = 0;
   let color = '';
-  
+  let newHeight = 0;
+  let newColor = '';
+  let first = false;
+
   for (let i = 0; i < 100; i++) {
-    let randomHeight = Math.floor(Math.random() * 30);
+    let randomHeight = Math.floor(Math.random() * 40 + 10);
     let reverse = Math.floor(Math.random() * 2);
-    
+
+    if (newColor !== color) {first = true} else first = false
+
     if (reverse) {
       color = 'green';
-      guide -= randomHeight;
+      guide -= (randomHeight + newHeight);
     } else {
-      color = 'red';   
+      color = 'red';
     }
 
     console.log(guide);
 
     document.querySelector('#chart-container').innerHTML += `
-      <div class="candle" style="background-color:${color};
+      <div class="candle" 
+      onclick="alert('This is a ' + this.style.backgroundColor + ' candle.')" 
+      style="background-color:${color};
       top: ${guide}px;
       height:${randomHeight}px; 
       left:${left}px">
     `;
 
     left += 11;
+    newHeight = randomHeight;
+    newColor = color;
   }
 }
 
