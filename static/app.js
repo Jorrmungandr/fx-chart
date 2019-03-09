79a5439992b69b9a11cd951e6aadc43710ce9e61
Yorrmungand <edgarmarques@hotmail.com.br>
2019-03-09 18:46:31 -0300

    Fixed bugs and add cnadle's tails

--- static/app.js
+++ static/app.js
@@ -1,32 +1,55 @@
-let canvas = document.querySelector('#chartContainer');
-let context = canvas.getContext('2d');
-let guide = 0;
-let newHeight = 0;
-let first = false;
+const canvas = document.querySelector('#chartContainer');
+const context = canvas.getContext('2d');
+
 let color = '';
+let position = 300;
+let previousHeight = 0;
+let previousReverse = 0;
+let previousPosition = 0;
+let width = 10;
 
 context.fillStyle = "#FF0000";
 
 const createGraph = () => {
-  for (let i = 10; i < 800; i += 12) {
-    let randomHeight = Math.floor(Math.random() * 30 + 10)
+  for (let i = 10; i < 3000; i += 12) {
+    let height = Math.floor(Math.random() * 30 + 10);
     let reverse = Math.floor(Math.random() * 2);
+    let higherValue = Math.floor(Math.random() * 30);
+    let lowerValue = Math.floor(Math.random() * 30);
 
-    // if (newColor !== color) {first = true} else first = false
+    color = reverse ? 'green' : 'red';
+
+    //Four conditions (10,11,00,01)
 
     if (reverse) {
-      color = 'green';
-      guide -= randomHeight;
+      if (!previousReverse) {
+        position = previousPosition + (previousHeight - height);
+      } else {
+        position -= height;
+      }
+    } else if (previousReverse) {
+      position = previousPosition;
     } else {
-      color = 'red';
+      position += previousHeight;
     }
-    
-    context.fillStyle = color;
-    context.fillRect( i , 20, 10, guide);
 
-    newHeight = randomHeight;
+    console.log(i);
+
+    let midsection = (width) / 2
+
+    context.strokeStyle = "#000000";
+    context.fillStyle = color;
+    context.fillRect(i, position, width, height);
+    context.moveTo(i + midsection, position - higherValue);
+    context.lineTo(i + midsection, position);
+    context.moveTo(i + midsection, position + height)
+    context.lineTo(i + midsection, position + height + lowerValue)
+
+    previousHeight = height;
+    previousReverse = reverse;
+    previousPosition = position;
   }
-  context.stroke(); 
+  context.stroke();
 
 }
 
