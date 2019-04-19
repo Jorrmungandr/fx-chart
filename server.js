c6861dc2edb34b7ad119521796c68fd51d29cbad
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-19 09:44:20 -0300

    Added Comments

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-let express = require('express');
-let app = express();
-let path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//Using "static" folder
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
+//Sending the html file
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'))
+    res.sendFile(path.join(__dirname, 'index.html'))
 });
 
 app.listen(3000);
--- static/app.js
+++ static/app.js
@@ -1,24 +1,36 @@
+//the canvas in html
 const canvas = document.querySelector('#chartContainer');
 const context = canvas.getContext('2d');
 
+//The color(If it's going up or down)
 let color = '';
+//Initial position
 let position = 300;
+//The stats of the previous candles
 let previousHeight = 0;
 let previousReverse = 0;
 let previousPosition = 0;
+//The width of the candles
 let width = 10;
 
 context.fillStyle = "#FF0000";
 
+//This is where we create the graphs
 const createGraph = () => {
   for (let i = 10; i < 3000; i += 12) {
+    //The "modulus" of the candle to be created
     let height = Math.floor(Math.random() * 30 + 10);
+    //If it's up or down
     let reverse = Math.floor(Math.random() * 2);
+    //The "Upper Stick" in the candles
     let higherValue = Math.floor(Math.random() * 30);
+    //The "Bottom Stick" in the candles
     let lowerValue = Math.floor(Math.random() * 30);
 
+    //Changing the colors 
     color = reverse ? 'green' : 'red';
 
+    //This is where de "candles" are distributed acordingly to it's type or condition
     //Four conditions (10,11,00,01)
 
     if (reverse) {
@@ -32,16 +44,22 @@ const createGraph = () => {
     } else {
       position += previousHeight;
     }
-
-    console.log(i);
-
+    //The midsection is the candle's width half
     let midsection = (width) / 2
 
+    //The stroke color is white
     context.strokeStyle = "#000000";
+    //The color of the candle (red or green)
     context.fillStyle = color;
+    //  Actually making the candle, where i and position are defined in the previous section,
+    //being summed up every time the program makes a new candle, so every candle is alongside
+    //each other
     context.fillRect(i, position, width, height);
+    //Making the sticks
+    //Upper Stick
     context.moveTo(i + midsection, position - higherValue);
     context.lineTo(i + midsection, position);
+    //Bottom Stick
     context.moveTo(i + midsection, position + height)
     context.lineTo(i + midsection, position + height + lowerValue)
 
@@ -49,8 +67,10 @@ const createGraph = () => {
     previousReverse = reverse;
     previousPosition = position;
   }
+  //Finally making the graph
   context.stroke();
 
 }
 
+//This is the eventListener to the "New Graph" Button
 document.querySelector('#createGraph').addEventListener('click', createGraph)
